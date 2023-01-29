76b01e59a6c5e07334cfb531e9b27fad18c27b0d
Ten Konstantin <kten@ozon.ru>
2023-01-29 18:25:45 +0300

    add serilog

--- Spydee3/Kos811.Spydee3.Api/Program.cs
+++ Spydee3/Kos811.Spydee3.Api/Program.cs
@@ -1,3 +1,5 @@
+using Serilog;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Host.UseSerilog((context, services, configuration) =>
+    configuration
+    .ReadFrom.Configuration(context.Configuration)
+    .ReadFrom.Services(services)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+);
 
 WebApplication app = builder.Build();
 
