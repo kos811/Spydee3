fdb425cd3567ad4bc938204312df44ca6265e2d9
Ten Konstantin <kten@ozon.ru>
2023-01-29 19:07:57 +0300

    some changes
    adding startup

---
+++ Kos811.Spydee3.Api/Startup.cs
@@ -0,0 +1,39 @@
+﻿using Kos811.Spydee3.Services;
+
+namespace Kos811.Spydee3.Api;
+
+public class Startup
+{
+    //private readonly IConfiguration _configuration;
+
+    //public Startup(IConfiguration configuration) =>_configuration = configuration;
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers()
+            .Services
+            .AddEndpointsApiExplorer()
+            .AddSwaggerGen();
+
+
+        services.AddTransient<StubService>();
+    }
+
+    public void Configure(IApplicationBuilder app, IHostEnvironment env)
+    {
+        app
+            .UseRouting()
+            .UseAuthentication()
+            .UseAuthorization()
+            .UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+        if (env.IsDevelopment())
+        {
+            _ = app.UseSwagger()
+                .UseSwaggerUI();
+        }
+    }
+}
---
+++ Kos811.Spydee3.Services/StubService.cs
@@ -0,0 +1,13 @@
+﻿namespace Kos811.Spydee3.Services;
+
+public class StubService
+{
+    public int GetAnswer(string question)
+    {
+        return question switch
+        {
+            string str when string.IsNullOrWhiteSpace(str) => throw new ArgumentOutOfRangeException(),
+            _ => 42
+        };
+    }
+}
