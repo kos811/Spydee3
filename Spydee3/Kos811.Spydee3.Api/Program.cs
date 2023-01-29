ca1145dedfc981d5b6a96ab6f985691e90438761
Ten Konstantin <kten@ozon.ru>
2023-01-29 18:03:29 +0300

    [KOS811-1] api project initial

---
+++ Spydee3/Kos811.Spydee3.Api/Program.cs
@@ -0,0 +1,22 @@
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+WebApplication app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    _ = app.UseSwagger()
+        .UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+//app.UseAuthorization();
+app.MapControllers();
+app.Run();
